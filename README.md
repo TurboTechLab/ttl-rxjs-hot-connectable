ffe0092c2789a599da64ff32a51892ac9958d645
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-22 12:23:41 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ttl-rxjs-hot-connectable
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ttl-rxjs-multicast-connectable-wpmdbn)
---
+++ index.ts
@@ -0,0 +1,20 @@
+import { Observable, Subject, connectable } from 'rxjs';
+
+const randomValue = () => Math.round(Math.random() * 10);
+
+const coldByDefault = new Observable((observer) => {
+  observer.next(randomValue());
+  observer.complete();
+});
+
+const hotWithConnectable = connectable(
+    coldByDefault, 
+    { connector: () => new Subject()}
+);
+
+//Now all the subscriptions will share the same data stream 
+hotWithConnectable.subscribe((v) => console.log(v)),
+hotWithConnectable.subscribe((v) => console.log(v)), 
+hotWithConnectable.subscribe((v) => console.log(v));
+//connect will start streaming the data
+hotWithConnectable.connect();
